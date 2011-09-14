132c7dcc3499464d79819278835783763055686a
samar-tmr <samar.tmr@gmail.com>
2011-09-14 23:08:18 +0530

    commiting new add

---
+++ MyVenture-BlackHawk/Factory/DataLayerSource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MyVenture;
+class MBlog
+{
+	public function GetMySubscribedMBlogs($uid)
+	{
+			return "testing";// testing the comments - changed at git hub site // new second comment
+	}
+
+	public function test(){
+		
+	}
+	
+	
+		public function tes2(){
+	}
+	
+	
+		public function tes3(){
+	}
+
+
+}
+
+class Connections
+{
+
+
+}
+
+class User
+{
+
+
+}
+
+class Ads
+{
+
+
+}
+
